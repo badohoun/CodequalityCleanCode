25e204fb1e10317705bce05c51cfb1a1021afc23
badohoun <espoir.badohoun@gmail.com>
2024-02-07 14:32:41 +0100

    commiting typehints py file v3

--- check-code-quality.sh
+++ check-code-quality.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#set -e  -> 
-# pip install black pylint flake8  mypy 
+#set -e  ->
+# pip install black pylint flake8  mypy
 
-EXIT_STATUS=0 
+EXIT_STATUS=0
 
 black --config .black.toml .  || ((EXIT_STATUS++))
 pylint --rcfile .pylintrc *.py type_hints || ((EXIT_STATUS++))
@@ -12,6 +12,5 @@ ruff . --config ruff.toml --fix  || ((EXIT_STATUS++))
 isort . --settings .isort.cfg || ((EXIT_STATUS++))
 
 
-echo exiting with status $EXIT_STATUS   
+echo exiting with status $EXIT_STATUS
 exit $EXIT_STATUS #  the idea is we could increment exit status if any of these commands failed
-
