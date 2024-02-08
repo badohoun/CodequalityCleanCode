41527a1481f03c4fe3cfd0eed709c0f6d1eadac9
badohoun <espoir.badohoun@gmail.com>
2024-02-08 08:17:32 +0100

    testing merge check

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 """Je suis un fichier test python."""
 
 
+# pylint:disable=invalid-name
 def add_numbers(a: int, b: int) -> int:
-    """Return the sum of two numbers."""
+    """Return the sum of two numbers a and b."""
     return sum([a, b])
